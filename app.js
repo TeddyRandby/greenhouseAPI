08a3153ec98031a7d1c65f8fd0b12eb405376334
Teddy <tedrandby@gmail.com>
2019-09-24 16:16:12 -0400

    some changes

--- app.js
+++ app.js
@@ -9,8 +9,32 @@ app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHTTP({
 
-  schema: null,
-  rootValue: {}
+  schema: buildSchema(`
+    type RootQuery {
+      users: [String!]!
+    }
+
+    type RootMutation {
+      createUser(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+
+  rootValue: {
+    events: () => {
+      return ['Watters', 'JMac', 'Teddy', 'Lucas', 'Kyle']
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  
+  graphiql: true
 }));
 
 app.listen(3000);
